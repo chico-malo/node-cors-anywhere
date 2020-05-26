21ed96b4e17937fe57705ed5582c23cd6acff12a
yangyao <873242789@qq.com>
2020-05-26 16:37:52 +0800

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yyao(873241789@qq.com)
+ * date: 2020-05-20
+ */
+var host = process.env.HOST || "0.0.0.0";
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require("cors-anywhere");
+cors_proxy
+    .createServer({
+        originWhitelist: [], // Allow all origins
+        requireHeader: ["origin", "x-requested-with"],
+        removeHeaders: ["cookie", "cookie2"]
+    })
+    .listen(port, host, function () {
+        console.log("Running CORS Anywhere on " + host + ":" + port);
+    });
